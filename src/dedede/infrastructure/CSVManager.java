115c9678d5f98217f8d697d8ea1a94ceaca53ef8
Tau <tau0@tutanota.com>
2025-10-23 13:04:06 +0200

    initial fix for CSVManager

--- src/dedede/infrastructure/CSVManager.java
+++ src/dedede/infrastructure/CSVManager.java
@@ -1,24 +1,33 @@
 package dedede.infrastructure;
 
 import java.io.*;
+import java.util.ArrayList;
 import java.util.List;
 
 public class CSVManager {
     File file;
-    BufferedReader input;
+    ArrayList<CSVRow> rows;
+    CSVRow header;
 
     public CSVManager(File file) throws IOException {
-        input = new BufferedReader(new FileReader(file));
         this.file = file;
+        try (var reader = new BufferedReader(new FileReader(file))) {
+            var lines = reader.lines();
+            this.header = CSVRow.FromLine(lines.findFirst().get());
+            this.rows = new ArrayList<>(lines.map(CSVRow::FromLine).toList());
+        }
     }
 
     public List<CSVRow> listAll() {
+        try {
+            input.close();
+            input = new BufferedReader(new FileReader(file));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
         var lines = input.lines();
         // Convert every line of the csv file to CSVRow with CSVRow.FromLine
-        return lines
-                .skip(1) // Skip header of csv
-                .map(CSVRow::FromLine)
-                .toList();
+        return
     }
 
     public void insertRow(CSVRow csvRow) throws IOException {
@@ -30,22 +39,36 @@ public class CSVManager {
     }
 
     public void updateRow(String ID, int column, CSVRow csvRow) throws IOException {
+        input.close();
+        input = new BufferedReader(new FileReader(file));
+        var lines = input.lines().toList();
+        input.close();
         try(BufferedWriter output = new BufferedWriter(new FileWriter(file))) {
-            var lines = input.lines();
-            lines.map(CSVRow::FromLine).toList().forEach(line -> {
+            lines.stream().map(CSVRow::FromLine).forEach(line -> {
+                System.out.println(line.toLine());
                 if (line.fields.get(column).equals(ID)) {
                     try {
                         output.write(csvRow.toLine());
+                        output.newLine();
+                    } catch (IOException e) {
+                        throw new RuntimeException(e);
+                    }
+                } else {
+                    try {
+                        output.write(line.toLine());
+                        output.newLine();
                     } catch (IOException e) {
                         throw new RuntimeException(e);
                     }
                 }
-                try {
-                    output.flush();
-                } catch (IOException e) {
-                    throw new RuntimeException(e);
-                }
+
             });
+            try {
+                output.flush();
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
         }
+        input.close();
     }
 }
