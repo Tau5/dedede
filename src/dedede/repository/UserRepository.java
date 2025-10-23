fc315f797bbc8a01a8c67079635b4d00534652a7
Chuwi12 <srpc9682@protonmail.com>
2025-10-23 14:16:51 +0200

    Add UserRepository.java class

---
+++ src/dedede/repository/UserRepository.java
@@ -0,0 +1,45 @@
+package dedede.repository;
+
+import dedede.domain.User;
+import dedede.infrastructure.CSVManager;
+
+public class UserRepository implements IRepositorio<User, Long> {
+
+    private CSVManager table;
+
+    @Override
+    public long count() {
+        return (long) table.listAll().size() - 1;
+    }
+
+    @Override
+    public void deleteById(Long aLong) {
+        table.deleteRow(l);
+    }
+
+    @Override
+    public void deleteAll() {
+        table.emptyTable();
+    }
+
+    @Override
+    public boolean existsById(Long aLong) {
+        return false;
+    }
+
+    @Override
+    public User findById(Long aLong) {
+        return null;
+    }
+
+    @Override
+    public Iterable findAll() {
+        return null;
+    }
+
+    @Override
+    public <S extends User> S save(S entity) {
+        return null;
+    }
+
+}
