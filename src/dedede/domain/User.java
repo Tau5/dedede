5200b802ac1b9c9684ef37d7c3ba08d4d979586c
Chuwi12 <srpc9682@protonmail.com>
2025-10-24 11:12:28 +0200

    Change User.java from record to class

--- src/dedede/domain/User.java
+++ src/dedede/domain/User.java
@@ -2,7 +2,41 @@ package dedede.domain;
 
 import java.time.Instant;
 
-public record User(Long ID, String name, String surname){
+public class User {
+
+    private Long ID;
+    private String name;
+    private String surname;
+
+    public User(Long ID, String name, String surname) {
+        this.ID = ID;
+        this.name = name;
+        this.surname = surname;
+    }
+
+    public Long getID() {
+        return ID;
+    }
+
+    public void setID(Long ID) {
+        this.ID = ID;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
 
     public void borrowBook(Book book) {
         book.setBorrowed(true);
