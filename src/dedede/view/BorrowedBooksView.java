f00fc3bc1b1ae623f7550c11a49f62f988f6fb56
Chuwi12 <srpc9682@protonmail.com>
2025-10-30 14:05:47 +0100

    Add BorrowBooksView.java class

---
+++ src/dedede/view/BorrowedBooksView.java
@@ -0,0 +1,23 @@
+package dedede.view;
+
+import dedede.domain.User;
+
+public class BorrowedBooksView extends View {
+    
+    private User user;
+
+    public BorrowedBooksView(Model model, ViewManager viewManager, User user) {
+        super(model, viewManager);
+        this.user = user;
+    }
+
+    @Override
+    void run() {
+        var books = model.books;
+        books.findAllList().forEach((book) -> {
+            if (book.getUserID() == user.getID()) {
+                System.out.println(book);
+            }
+        });
+    }
+}
