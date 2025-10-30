8e1a5b809562c0b291efa401db15a3b5d51448c8
Chuwi12 <srpc9682@protonmail.com>
2025-10-30 14:06:26 +0100

    Add BorrowBooksView.java class

--- src/dedede/view/BorrowBooksView.java
+++ src/dedede/view/BorrowBooksView.java
@@ -2,14 +2,27 @@ package dedede.view;
 
 import dedede.domain.User;
 
-public class BorrowBooksView extends View {
+public class BorrowBooksView extends View{
+
+    private User user;
 
     public BorrowBooksView(Model model, ViewManager viewManager, User user) {
         super(model, viewManager);
+        this.user = user;
     }
 
     @Override
     void run() {
-
+        var books = model.books;
+        var id = MenuHelper.getNumber("Ingrese el id del libro que quiere prestar");
+        books.findAllList().forEach((book) -> {
+            if (book.getID() == id) {
+                if (book.isBorrowed() != true) {
+                    user.borrowBook(book);
+                } else {
+                    System.out.println("No se puede prestar el libro, ya esta prestado");
+                }
+            }
+        });
     }
 }
