50ae481c2d06992626872d201dd571b1629e8d63
Chuwi12 <srpc9682@protonmail.com>
2025-10-31 10:55:15 +0100

    Refactor Code

--- src/dedede/view/usuario/UserHomeView.java
+++ src/dedede/view/usuario/UserHomeView.java
@@ -17,7 +17,7 @@ public class UserHomeView implements View {
         menu.registerOption(1, "Ver libros prestados", () -> new BorrowedBooksView(user));
         menu.registerOption(2, "Pedir libro prestado", () -> new BorrowBooksView(user));
         menu.registerOption(3, "Devolver libro", () -> new ReturnBookView(user));
-        menu.registerOption(4, "Eliminar cuenta", () -> null);
+        menu.registerOption(4, "Eliminar cuenta", () -> new DeleteUserView(user));
         menu.registerOption(5, "Salir", ViewModo::new);
 
         View nextView = menu.chooseAndExecute("opcion: ");
