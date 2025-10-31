21452c7443b59d9a8ba1846a46a95932422cf120
Tau <tau0@tutanota.com>
2025-10-31 10:04:20 +0100

    BorrowedBooksView: Only print books that are marked as borrowed
    (cherry picked from commit 2e6aa45a93636b5f7e494539afc92f0b607cdee3)
    
    # Conflicts:
    #	src/dedede/view/BorrowedBooksView.java

--- src/dedede/view/BorrowedBooksView.java
+++ src/dedede/view/BorrowedBooksView.java
@@ -11,8 +11,8 @@ public class BorrowedBooksView implements View {
     @Override
     public void run(Model model, ViewManager viewManager) {
         var books = model.books;
-        books.findAllList().forEach(book -> {
-            if (book.getUserID() == user.getID()) {
+        books.findAllList().forEach((book) -> {
+            if (book.getUserID() == user.getID() && book.isBorrowed()) {
                 System.out.println(book);
             }
         });
