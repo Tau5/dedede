f96f729e10aa8ea31582fae91a2510462d8a7997
Tau <tau0@tutanota.com>
2025-11-06 13:49:44 +0100

    fix getting stuck in ViewRegistrarse

--- src/dedede/view/usuario/ViewRegistrarse.java
+++ src/dedede/view/usuario/ViewRegistrarse.java
@@ -11,7 +11,6 @@ public class ViewRegistrarse implements View {
     public void run(Model model, ViewManager viewManager) {
         MenuHelper.sc.reset();
         System.out.print("nombre: ");
-        MenuHelper.sc.nextLine();
         String nombre = MenuHelper.sc.nextLine();
         System.out.print("apellidos: ");
         String apellidos = MenuHelper.sc.nextLine();
