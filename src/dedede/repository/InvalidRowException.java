e7400f2dc48f92b383022c062023c5a4201dfb42
Chuwi12 <srpc9682@protonmail.com>
2025-11-06 13:29:47 +0100

    Add constructor to InvalidRowException.java

--- src/dedede/repository/InvalidRowException.java
+++ src/dedede/repository/InvalidRowException.java
@@ -5,4 +5,8 @@ import java.io.Serial;
 public class InvalidRowException extends Exception {
     @Serial
     private static final long serialVersionUID = -8631699000166883645L;
+
+    public InvalidRowException(String msg) {
+        super(msg);
+    }
 }
